d39495ca0ce26752b91d1117df9fa6fe7cd0012c
Devmadia <agathaphan@icloud.com>
2020-08-26 22:17:37 -0500

    working theory of index.js relationship models with User, Card, Project, Comment

--- models/index.js
+++ models/index.js
@@ -1,4 +1,30 @@
 const User = require('./User');
+const Project = require('./Project');
+
+// require the Post model
+const Card = require("./Card");
+const Comment = require('./Comments');
+
+
+User.hasMany(Project);
+
+Project.belongsToMany(User);
+
+User.hasMany(Card);
+
+Card.belongsToMany(User);
+
+User.hasMany(Comment);
+
+Comment.belongsToMany(User);
+
+Card.hasMany(Comment);
+
+Comment.belongsTo(Card);
+
+Project.hasMany(Card);
+
+Card.belongsTo(Project)
 
 // import user model data
-module.exports = { User }; 
+module.exports = { User, Comment, Project, Card }; 
